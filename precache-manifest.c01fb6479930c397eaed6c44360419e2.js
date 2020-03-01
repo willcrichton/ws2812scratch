ed7d57b4e397d4454b5de017897742d6a5c0e179
Will Crichton <wcrichto@cs.stanford.edu>
2020-02-29 19:57:12 -0800

    Fix path

---
+++ precache-manifest.c01fb6479930c397eaed6c44360419e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f71532776aeb3238b85b4dd70a4f7b",
+    "url": "/ws2812scratch/index.html"
+  },
+  {
+    "revision": "8ebef5e67c548c9b6e1a",
+    "url": "/ws2812scratch/static/css/main.f1a079f4.chunk.css"
+  },
+  {
+    "revision": "f1e7b94118e0b5bed524",
+    "url": "/ws2812scratch/static/js/2.16484fd6.chunk.js"
+  },
+  {
+    "revision": "c1ada934c8615759f028d71cdb05e5f8",
+    "url": "/ws2812scratch/static/js/2.16484fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebef5e67c548c9b6e1a",
+    "url": "/ws2812scratch/static/js/main.63a63382.chunk.js"
+  },
+  {
+    "revision": "3d9d86ba0429a811040b",
+    "url": "/ws2812scratch/static/js/runtime-main.54d898d4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./ws2812scratch/precache-manifest.923d03b522e636e60d652e094fb20138.js"
+  "/ws2812scratch/precache-manifest.c01fb6479930c397eaed6c44360419e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./ws2812scratch/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ws2812scratch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
